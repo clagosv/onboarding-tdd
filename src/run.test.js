0c63b695f6669d09d1f7b86183274b470a846e3e
@sergio.gonzalez5 <sergio.gonzalez@thoughtworks.com>
2021-05-04 18:13:25 -0400

    limpiando proyecto

--- src/run.test.js
+++ src/run.test.js
@@ -2,21 +2,4 @@ import { run } from './run';
 
 test('Should return 0 if string is empty', () => {
   const result = run('');
-  expect(result).toEqual(0);
-});
-
-test("Should return 1 when string is '1'", () => {
-  const result = run('1');
-  expect(result).toEqual(1);
-});
-
-
-test("Should return 3 when imput is '1,2", () =>{
-  const result = run('1,2');
-  expect(result).toEqual(3);
-});
-
-test("Should return 4 when imput is '1,2,1", () =>{
-  const result = run('1,2,1');
-  expect(result).toEqual(4);
 });
