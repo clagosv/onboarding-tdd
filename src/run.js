eea8d5dbe7635a6ed48d90565a25b24db3a81b58
clagosv <carolina.lagos@thoughtworks.com>
2021-05-06 13:49:35 -0400

    dempty folder for onboarding

--- src/run.js
+++
@@ -1,3 +0,0 @@
-export const run = (args) =>{
-    console.log('hola mundo');
-};
--- src/run.test.js
+++
@@ -1,5 +0,0 @@
-import { run } from './run';
-
-test('Should return 0 if string is empty', () => {
-  const result = run('');
-});
